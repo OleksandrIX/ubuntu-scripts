a3b958d43fd9dffcfcd3aa88a2b2dba7805090e8
OleksandrIX <zagorenkosasha@gamil.com>
2023-11-05 12:10:12 +0200

    (fix) change java install script

--- java-install.sh
+++ java-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y openjdk-11-jdk
+
+java_path=$(update-alternatives --query java | grep "Best: " | awk '{print $2}')
+
+echo "export JAVA_HOME=$java_path" >> ~/.bashrc
+source ~/.bashrc
+
 java -version
 echo "Java 11 is installed."
+echo "JAVA_HOME is set to $java_path."
